53fa723316dd604c82bd2fa58c92987f0af2f64f
yzx <tianjiangziwu@vip.qq.com>
2017-12-20 15:54:35 +0800

    添加读取

---
+++ Assets/Editor/UF3DMenu.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.IO;
+
+public class UF3DMenu
+{
+    private static string sourceFile = string.Empty;
+    private static string sourceFileName = string.Empty;
+    private static string sourcePath = string.Empty;
+
+    [MenuItem("WDWebTools/导入特效", false, 0)]
+    public static void OpenDiagle()
+    {
+        if (OpenUf3dFile())
+        {
+            Uf3dLoader loader = new Uf3dLoader();
+            loader.parse(sourceFile);
+        }
+    }
+
+    public static bool OpenUf3dFile()
+    {
+        OpenFileName ofn = new OpenFileName();
+        ofn.structSize = Marshal.SizeOf(ofn);
+        ofn.filter = "(*.uf3d)\0*.uf3d";
+        ofn.file = new string(new char[1024]);
+        ofn.maxFile = ofn.file.Length;
+        ofn.fileTitle = new string(new char[512]);
+        ofn.maxFileTitle = ofn.fileTitle.Length;
+        ofn.initialDir = UnityEngine.Application.dataPath;//默认路径  
+        ofn.title = "选择特效文件";
+        ofn.flags = 0x00080000 | 0x00001000 | 0x00000800 | 0x00000008;//OFN_EXPLORER|OFN_FILEMUSTEXIST|OFN_PATHMUSTEXIST|OFN_NOCHANGEDIR  
+        if (DllTest.GetOpenFileName(ofn))
+        {
+            copyData(ofn);
+            return true;
+        }
+        return false;
+    }
+
+    private static void copyData(OpenFileName ofn)
+    {
+        sourceFile = ofn.file;
+        sourceFileName = ofn.fileTitle;
+        sourcePath = ofn.file.Substring(0, ofn.file.IndexOf(ofn.fileTitle));
+    }
+}
---
+++ Assets/Editor/Uf3dLoader.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+public class Uf3dLoader {
+
+    private string _strFileName = string.Empty;
+
+    public string StrFileName
+    {
+        get
+        {
+            return _strFileName;
+        }
+
+        set
+        {
+            _strFileName = value;
+        }
+    }
+
+    public bool parse(string path)
+    {
+        if (System.IO.Path.GetExtension(path).ToLower() == ".uf3d")
+        {
+            if (System.IO.File.Exists(path) == true)
+            {
+                this._strFileName = path;
+                MemoryStream filestream = null;
+                BinaryReader binReader = null;
+                byte[] filebytes = null;
+
+                // load the file as an array of bytes
+                filebytes = System.IO.File.ReadAllBytes(this._strFileName);
+                if (filebytes != null && filebytes.Length > 0)
+                {
+                    // create a seekable memory stream of the file bytes
+                    using (filestream = new MemoryStream(filebytes))
+                    {
+                        if (filestream != null && filestream.Length > 0 && filestream.CanSeek == true)
+                        {
+                            // create a BinaryReader used to read the Targa file
+                            using (binReader = new BinaryReader(filestream))
+                            {
+                                //this.LoadTGAFooterInfo(binReader);
+                                //this.LoadTGAHeaderInfo(binReader);
+                                //this.LoadTGAExtensionArea(binReader);
+                                //this.LoadTGAImage(binReader);
+                            }
+                        }
+                        else
+                            throw new Exception(@"Error loading file, could not read file from disk.");
+
+                    }
+                }
+                else
+                    throw new Exception(@"Error loading file, could not read file from disk.");
+            }
+            else
+                throw new Exception(@"Error loading file, could not find file '" + _strFileName + "' on disk.");
+        }
+        else
+            throw new Exception(@"Error loading file, file '" + _strFileName + "' must have an extension of '.uf3d'.");
+
+        return true;
+    }
+}
